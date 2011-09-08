87fd855763c887410f452fbbb3e440e6d7e18488
kenneth.b.kent@gmail.com <kenneth.b.kent@gmail.com@fe151cf2-c8a8-e2d8-b33d-a4858693285b>
2011-09-08 18:50:35 +0000

    Fixed a bug with padding nodes

--- ODIN_II/ODIN_II/SRC/partial_map.c
+++ ODIN_II/ODIN_II/SRC/partial_map.c
@@ -86,6 +86,7 @@ void depth_first_traversal_to_partial_map(short marker_value, netlist_t *netlist
 	/* now traverse the ground and vcc pins */
 	depth_first_traverse_parital_map(netlist->gnd_node, marker_value, netlist);
 	depth_first_traverse_parital_map(netlist->vcc_node, marker_value, netlist);
+	depth_first_traverse_parital_map(netlist->pad_node, marker_value, netlist);
 }
 
 /*---------------------------------------------------------------------------------------------
@@ -243,6 +244,7 @@ void partial_map_node(nnode_t *node, short traverse_number, netlist_t *netlist)
 		case GND_NODE:
 		case VCC_NODE:
 		case FF_NODE:
+		case PAD_NODE:
 			/* some nodes already in the form that is mapable */
 			break;
 
