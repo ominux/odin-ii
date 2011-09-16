d3fd677655595816155cdbaa908be5d09875f42d
andy16666@gmail.com <andy16666@gmail.com@fe151cf2-c8a8-e2d8-b33d-a4858693285b>
2011-09-16 12:45:53 +0000

    Rewrote the queue for the new simulator.

--- trunk/ODIN_II/ODIN_II/SRC/queue.h
+++ trunk/ODIN_II/ODIN_II/SRC/queue.h
@@ -1,26 +1,55 @@
-#define queue_t struct queue_t_t
-#define queue_node_t struct queue_node_t_t
+/*
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
 
-#ifndef SIM_QUEUE
-#define SIM_QUEUE
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+#ifndef QUEUE_H
+#define QUEUE_H
+#define queue_t       struct queue_t_t
+#define queue_node_t  struct queue_node_t_t
+
+// Constructor 
 queue_t* create_queue();
-void destroy_queue(queue_t *q);
-void enqueue_item(queue_t *q, void *item);
-void* dequeue_item(queue_t *q);
-int is_empty (queue_t *q);
 
-struct queue_node_t_t
-{
+struct queue_t_t {
+	queue_node_t *head;
+	queue_node_t *tail;
+	int count;
+
+	void   (*add)        (queue_t *q, void *item); 
+	void  *(*remove)     (queue_t *q); 
+	void **(*remove_all) (queue_t *q); 
+	int    (*is_empty)   (queue_t *q); 
+	void   (*destroy)    (queue_t *q); 	
+};
+
+struct queue_node_t_t {
 	queue_node_t *next;
 	void *item;
 };
 
-struct queue_t_t
-{
-	queue_node_t *head;
-	queue_node_t *tail;
-	int count;
-};
+// Private
+void   ___queue_add(queue_t *q, void *item);
+void  *___queue_remove(queue_t *q);
+void **___queue_remove_all(queue_t *q);
+int    ___queue_is_empty (queue_t *q);
+void   ___queue_destroy(queue_t *q);
 
 #endif
