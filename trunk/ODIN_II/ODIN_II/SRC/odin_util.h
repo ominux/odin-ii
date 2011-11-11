4f9387f3e0f673243f9ff915e4fe399a5456315f
andy16666 <andy16666@fe151cf2-c8a8-e2d8-b33d-a4858693285b>
2011-11-11 19:58:55 +0000

    Added support for indefinitely long hex, octal and binary numbers. Extended the supported length of binary numbers to the length of a long long. Improved detection of malformed constants.

--- trunk/ODIN_II/ODIN_II/SRC/odin_util.h
+++ trunk/ODIN_II/ODIN_II/SRC/odin_util.h
@@ -8,11 +8,11 @@ char *make_signal_name(char *signal_name, int bit);
 char *make_full_ref_name(char *previous, char *module_name, char *module_instance_name, char *signal_name, int bit);
 
 char *twos_complement(char *str);
-char *convert_long_to_bit_string(long long orig_long, int num_bits);
+char *convert_long_long_to_bit_string(long long orig_long, int num_bits);
 long long convert_dec_string_of_size_to_long(char *orig_string, int size);
-long long convert_hex_string_of_size_to_long(char *orig_string, int size);
-long long convert_oct_string_of_size_to_long(char *orig_string, int size);
-long long convert_binary_string_of_size_to_long(char *orig_string, int size);
+char *convert_hex_string_of_size_to_bit_string(char *orig_string, int size);
+char *convert_oct_string_of_size_to_bit_string(char *orig_string, int size);
+char *convert_binary_string_of_size_to_bit_string(char *orig_string, int size);
 
 long long int my_power(long long int x, long long int y);
 long long int pow2(int to_the_power);
@@ -21,6 +21,13 @@ char *make_string_based_on_id(nnode_t *node);
 char *make_simple_name(char *input, char *flatten_string, char flatten_char);
 
 void *my_malloc_struct(int bytes_to_alloc);
+
+void string_reverse(char *token, int length);
+
+int is_binary_string(char *string);
+int is_octal_string(char *string);
+int is_decimal_string(char *string);
+int is_hex_string(char *string);
 
 #endif
 
