832750fcb7a0edb55857aa7725349c77659a799b
andy16666@gmail.com <andy16666@gmail.com@fe151cf2-c8a8-e2d8-b33d-a4858693285b>
2011-11-18 20:16:37 +0000

    Categorised and diagnosed microbenchmarks. Added known good input and output vector sets for benchmarks which are known to agree with modelsim. Added a script to self-test odin using known good benchmarks and input and output vector sets.

--- ODIN_II/ODIN_II/REGRESSION_TESTS/BENCHMARKS/MICROBENCHMARKS/bm_DL_BCD_adder.v
+++ ODIN_II/ODIN_II/REGRESSION_TESTS/BENCHMARKS/MICROBENCHMARKS/bm_DL_BCD_adder.v
@@ -1,12 +1,13 @@
 /* Pg 287 - BCD adder */
 
-module bm_DL_BCD_adder (Cin, X, Y, S, Cout);
+module bm_DL_BCD_adder (Cin, X, Y, S, Cout, Z);
 	input Cin;
 	input [3:0] X, Y;
 	output [3:0] S;
 	output Cout;
 	reg [3:0] S;
 	reg Cout;
+	output [4:0] Z;
 	reg [4:0] Z;
 	
 	always @(X or Y or Cin)
