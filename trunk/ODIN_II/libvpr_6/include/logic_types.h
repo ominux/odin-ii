6b77920eb2641fef1011d9eb1009890279e87053
JasonKaiLuu@gmail.com <JasonKaiLuu@gmail.com@fe151cf2-c8a8-e2d8-b33d-a4858693285b>
2012-01-30 19:22:41 +0000

    Finished up commenting of fpga arch

--- trunk/ODIN_II/libvpr_6/include/logic_types.h
+++ trunk/ODIN_II/libvpr_6/include/logic_types.h
@@ -1,5 +1,6 @@
 /*
-Data types describing the logic models that the architecture can implement.
+Data types describing the logic (technology-mapped) models that the architecture can implement.
+Logic models include LUT (.names), flipflop (.latch), inpad, outpad, memory slice, etc
 
 Date: February 19, 2009
 Authors: Jason Luu and Kenneth Kent
@@ -14,29 +15,28 @@ Authors: Jason Luu and Kenneth Kent
 Logic model data types
 A logic model is described by its I/O ports and function name
 */
-
 enum PORTS {IN_PORT, OUT_PORT, INOUT_PORT, ERR_PORT};
 typedef struct s_model_ports
 {
-	enum PORTS dir;
-	char *name;
-	int size;
-	int min_size;
-	boolean is_clock;
-	struct s_model_ports *next;
+	enum PORTS dir; /* port direction */
+	char *name;		/* name of this port */
+	int size;		/* maximum number of pins */
+	int min_size;   /* minimum number of pins */
+	boolean is_clock; /* clock? */
+	struct s_model_ports *next; /* next port */
 
-	int index;
+	int index;      /* indexing for array look-up */
 } t_model_ports;
 
 typedef struct s_model
 {
-	char *name;
-	t_model_ports *inputs;
-	t_model_ports *outputs;
-	void *instances;
-	int used;
-	struct s_linked_vptr *pb_types;
-	struct s_model *next;
+	char *name;						/* name of this logic model */
+	t_model_ports *inputs;			/* linked list of input/clock ports */
+	t_model_ports *outputs;			/* linked list of output ports */
+	void *instances;				
+	int used;						
+	struct s_linked_vptr *pb_types; /* Physical block types that implement this model */
+	struct s_model *next;			/* next model (linked list) */
 
 	int index;
 } t_model;
